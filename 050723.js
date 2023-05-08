3157932c711945ac7170d879e92a686086593946
quinnrkemp <a467814@gmail.com>
2023-05-07 23:49:07 -0400

    coding chall for 0507923

---
+++ 050723.js
@@ -0,0 +1,29 @@
+// CHALLENGE NAME-  Bin to Decimal
+
+// DESCRIPTION:
+
+// Complete the function which converts a binary number 
+// (given as a string) to a decimal number.
+
+// // ***STARTER CODE***
+
+// function binToDec(bin){
+//     // ...
+//   } 
+
+// //   ******TEST CASES*****
+
+// describe("Example Tests", () => {
+//     [ ["1",1], ["0",0], ["1001001", 73] ].forEach( ([inp, exp]) => 
+//     Test.assertEquals( binToDec(inp), exp ) )
+//   }); 
+
+// ******MY ANSWER********
+
+function binToDec(bin){
+    return parseInt(bin,2)
+    
+    // ...
+   }
+   
+   // ***answer with the help of the solutions***
